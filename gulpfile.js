b796c47736abb99dbd4a7f1d47bd4f6fdc9dd19f
Wei-Jye Wang <weijye91@live.com>
2015-07-20 12:35:30 +0800

    Update browser-sync gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,47 @@
-/**
- * Created by Wei-Jye on 7/12/2015.
- */
 (function() {
     'use strict';
     var gulp = require('gulp');
     var browserSync = require('browser-sync').create();
     var path = require('path');
     var karma = require('karma');
-    var Server = require('karma').Server;
 
-    function browserSyncTask() {
+    gulp.task('browser-sync', function(cb) {
         browserSync.init({
             server: {
                 baseDir: './src',
-                files: ['index.html', 'app.js', '**/*.html', '**/*.js', '**/*.css'],
+                files: ['**/*.html', '**/*.js', '**/*.css'],
                 port: 8000,
                 // Not working for some reason....
                 browser: ['internet explorer', 'google chrome'],
                 logLevel: 'debug'
             }
-        });
+        }, cb);
+    });
 
-        browserSync.reload({
-          stream: true   //
-        });
-
-        gulp.watch('src/index.html', function(event, file) {
-            browserSync.reload('src/index.html');
-        });
-        gulp.watch('src/app.js', function() {
-            browserSync.reload('src/app.js');
-        });
-        gulp.watch('src/**/*.js', function(event, file) {
-            // Extract the relative path from event.path and reload only those files.
+    gulp.task('watch', [
+        'browser-sync'
+    ], function() {
+        gulp.watch('src/**/*.html', function(event) {
             browserSync.reload(path.relative(__dirname, event.path));
         });
-        gulp.watch('src/*.css', function(event, file){
+        gulp.watch('src/**/*.js', function(event) {
             browserSync.reload(path.relative(__dirname, event.path));
         });
-    }
+        gulp.watch('src/**/*.css', function(event){
+            browserSync.reload(path.relative(__dirname, event.path));
+        });
+    });
 
-    gulp.task('default', browserSyncTask);
-    gulp.task('browser-sync', browserSyncTask);
-    gulp.task('test', function (done) {
-	  new Server({
-	    configFile: __dirname + '/karma.conf.js',
-	    singleRun: true
-	  }, done).start();
-	});
+    gulp.task('test', function(cb) {
+        new karma.Server({
+            configFile: __dirname + '/karma.conf.js',
+            singleRun: true
+        }, cb).start();
+    });
+
+    gulp.task('default', [
+        'watch'
+    ]);
 
     // Trying out browserify builds + watchify.
     //var watchify = require('watchify');
